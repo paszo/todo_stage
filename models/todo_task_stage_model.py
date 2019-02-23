2c79bddc79cb7c438e4abb5da80f9e63f09db9db
Paszo <paszo2008@gmail.com>
2019-02-23 21:54:34 +0100

    [ADD] Added some fields to the stage model

--- models/todo_task_stage_model.py
+++ models/todo_task_stage_model.py
@@ -5,5 +5,26 @@ class Stage(models.Model):
     _description = 'To-do Stage'
     _order = 'sequence, name'
 
+    # String fields
     name = fields.Char('Name', translate=True)
+    desc = fields.Text('Description')
+    state = fields.Selection(
+        [('draft', 'New'),
+        ('open', 'Started'),
+        ('done', 'Closed')],
+        'State')
+    docs = fields.Html('Documentation')
+
+    # Numeric fields
     sequence = fields.Integer('Sequence')
+    perc_complete = fields.Float('% Complete', (3,2))
+
+    # Date fields
+    date_effective = fields.Date('Effective Date')
+    date_created = fields.Datetime(
+        'Create Date and Time',
+        default=lambda self: fields.Datetime.now())
+
+    # Other fields
+    fold = fields.Boolean('Folded?')
+    image = fields.Binary('Image')
