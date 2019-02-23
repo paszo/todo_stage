42533be8fdd7268e5d168e038d2fd530b091a8b6
Paszo <paszo2008@gmail.com>
2019-02-23 22:23:46 +0100

    [ADD] Added a hover to the field

--- models/todo_task_model.py
+++ models/todo_task_model.py
@@ -3,3 +3,4 @@ from odoo import api, fields, models
 class TodoTask(models.Model):
     _inherit = 'todo.task'
     effort_estimate = fields.Integer()
+    name = fields.Char(help="What needs to be done?")
