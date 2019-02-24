2a6585f6de2a0914e58d03c7dc61ba277c219fdc
Paszo <paszo2008@gmail.com>
2019-02-24 07:11:17 +0100

    [ADD] Added relational fields to todo model

--- models/todo_task_model.py
+++ models/todo_task_model.py
@@ -4,3 +4,8 @@ class TodoTask(models.Model):
     _inherit = 'todo.task'
     effort_estimate = fields.Integer()
     name = fields.Char(help="What needs to be done?")
+
+    # Relational fields
+    stage_id = fields.Many2one('todo.task.stage', 'Stage')
+    tag_ids = fields.Many2many('todo.task.tag', string='Tags')
+    
