e9f91385005ed46fb140a6ee202107f9f22214f9
Paszo <paszo2008@gmail.com>
2019-02-24 07:55:12 +0100

    [ADD] Added dynamic reference fields to todo.task model

--- models/todo_task_model.py
+++ models/todo_task_model.py
@@ -8,4 +8,7 @@ class TodoTask(models.Model):
     # Relational fields
     stage_id = fields.Many2one('todo.task.stage', 'Stage')
     tag_ids = fields.Many2many('todo.task.tag', string='Tags')
-    
+
+    refers_to = fields.Reference(
+        [('res.user', 'User'), ('res.partner', 'Partner')],
+        'Refers to') 
