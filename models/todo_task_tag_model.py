14c55b29e9fb05d4ee34dcadb9adfec4f163aff5
Paszo <paszo2008@gmail.com>
2019-02-24 07:42:26 +0100

    [ADD] Added chierarchical relation to tag model

--- models/todo_task_tag_model.py
+++ models/todo_task_tag_model.py
@@ -7,3 +7,17 @@ class Tag(models.Model):
 
     # Many2many inverse relationship
     task_ids = fields.Many2many('todo.task', string = 'Tasks')
+
+    # Hierarchic relationships
+    _parent_store = True
+    # _parent_name = 'parent_id'  # the default
+    parent_id = fields.Many2one(
+        'todo.task.tag',
+        'Parent Tag',
+        ondelete='restrict')
+    parent_left = fields.Integer('Parent Left', index=True)
+    parent_right = fields.Integer('Parent Right', index=True)
+    child_ids = fields.One2many(
+        'todo.task.tag',
+        'parent_id',
+        'Child Tags')
