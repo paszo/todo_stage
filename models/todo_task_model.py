ac8f6bfa8bfe2884caac148ece9cc6a109148bc4
Paszo <paszo2008@gmail.com>
2019-02-24 08:19:18 +0100

    [ADD] Added searching and writing to computed fields

--- models/todo_task_model.py
+++ models/todo_task_model.py
@@ -17,9 +17,18 @@ class TodoTask(models.Model):
     # Computed fields
     stage_fold = fields.Boolean(
         'Stage Folded?',
-        compute='_compute_stage_fold')
+        compute='_compute_stage_fold',
+        search='_search_stage_fold',
+        inverse='_write_stage_fold')
 
     @api.depends('stage_id.fold')
     def _compute_stage_fold(self):
         for todo in self:
             todo.stage_fold = todo.stage_id.fold
+
+    def _search_stage_fold(self, operator, value):
+        return [('stage_id.fold', operator, value)]
+
+    def _write_stage_fold(self):
+        for todo in self:
+            todo.stage_id.fold = todo.stage_fold
